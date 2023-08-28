7d8e78b6f1f99deb14528e524e12b928ae4a420e
Cesar Guerrero <cguerrero@Cesars-MBP.attlocal.net>
2023-08-28 15:36:30 -0700

    In the end... It was all becuase of a misnamed environmental variable.

--- analytics_api/services/auth_service.py
+++ analytics_api/services/auth_service.py
@@ -17,32 +17,22 @@ from flask import session
 #This method is used to obtain the request tokens from Twitter in preperation for a user to sign in to our site via twitter
 async def obtain_twitter_request_token():
     endpoint_url = 'https://api.twitter.com/oauth/request_token'
-    print(os.getenv("API_KEY"))
-    print(os.getenv("API_SECRET"))
-    print(os.getenv("CALLBACK_URI"))
-    sys.stdout.flush()
 
     try:
         consumer = oauth.Consumer(os.getenv("API_KEY"), os.getenv("API_SECRET"))
         client = oauth.Client(consumer)
         response, content = client.request(endpoint_url, "POST", body=urllib.parse.urlencode({"oauth_callback": os.getenv("CALLBACK_URI")}))
         request_token = dict(urllib.parse.parse_qsl(content))
-        print(request_token)
-        sys.stdout.flush()
         
         if response['status'] != '200':
-            print("Failed to get token")
-            sys.stdout.flush()
             return False
 
         session['oauth_token'] = request_token[b'oauth_token'].decode('utf-8')
         session['oauth_token_secret'] = request_token[b'oauth_token_secret'].decode('utf-8')
         return True
     
-    except Exception as e:
+    except:
         #We failed the first-leg of OAuth1.0
-        print(e)
-        sys.stdout.flush()
         return False
 
 #This method completes the last leg of the identification process
@@ -50,13 +40,20 @@ async def obtain_twitter_access_token(oauth_verifier, session_token, session_sec
     endpoint_url = 'https://api.twitter.com/oauth/access_token'
 
     try:
-        #client = AsyncOAuth1Client(client_id=os.getenv("API_KEY"), client_secret=os.getenv("API_SECRET"))
-        #response = await client.fetch_access_token(endpoint_url, verifier=oauth_verifier, token=session_token, token_secret=session_secret)
+        consumer = oauth.Consumer(os.getenv("API_KEY"), os.getenv("API_SECRET"))
+        token = oauth.Token(session_token, session_secret)
+        token.set_verifier(oauth_verifier)
+        client = oauth.Client(consumer, token)
+
+        response, content = client.request(endpoint_url, "POST")
+        access_token = dict(urllib.parse.parse_qsl(content))
+        
         #Store these now verified keys in the session for future API calls
-        #session['auth_key'] = response.get('oauth_token')
-        #session['auth_secret'] = response.get('oauth_token_secret')
-        #session['username'] = response.get('screen_name')
-        #session['user_id'] = response.get('user_id')
+        session['auth_key'] = access_token[b'oauth_token'].decode('utf-8')
+        session['auth_secret'] = access_token[b'oauth_token_secret'].decode('utf-8')
+        session['username'] = access_token[b'screen_name'].decode('utf-8')
+        session['user_id'] = access_token[b'user_id'].decode('utf-8')
+
         return True
     except:
         return False
