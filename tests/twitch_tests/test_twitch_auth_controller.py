41365c306b642a9b024aa80b3f561a35df7319e3
Cesar Guerrero <cguerrero@Cesars-MBP.attlocal.net>
2023-09-28 20:06:18 -0700

    Readding base twitter and twitch tests

---
+++ tests/twitch_tests/test_twitch_auth_controller.py
@@ -0,0 +1,36 @@
+'''
+Cesar Guerrero
+09/27/23
+
+Testing our Twitch Authorization Controller
+'''
+
+import pytest
+from unittest.mock import AsyncMock, patch
+
+#Testing our 'Login with Twitch' Route
+class TestLoginClass:
+
+    def test_login_response(self,client, monkeypatch):
+        monkeypatch.setenv("TWITCH_API_KEY",'abc')
+
+        #You should not be able to call login with anything other than GET
+        assert client.post('/login/twitch').status_code == 405
+        assert client.get('/login/twitch').status_code == 200
+
+        #Testing that we correctly receiving a response from our function call
+        response = client.get('/login/twitch')
+        assert response.data == b'{"client_id":"abc","oauth_ready":true,"status_code":200,"status_message":"OK"}\n'
+
+class TestCallbackClass:
+
+    def test_null_code_callback(self, client):
+        response = client.get('/callback/twitch')
+        assert response.status_code == 401
+        assert response.data == b'Unauthorized'
+
+    @patch('analytics_api.services.twitch_auth_service.obtain_twitch_access_token', new=AsyncMock(return_value={"oauth_approved": True, "status_code": 200, "status_message": "OK"}))
+    def test_callback_response(self, client):
+        response = client.get('/callback/twitch?code=%s' %('abc123'))
+        assert response.data == b'{"oauth_approved":true,"status_code":200,"status_message":"OK"}\n'
+    
