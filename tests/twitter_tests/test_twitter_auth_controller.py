eaf871fd003493fd5cb9eb3a37ce9b5501922bb5
Cesar Guerrero <cguerrero@Cesars-MBP.attlocal.net>
2023-09-28 21:23:54 -0700

    I think this will work?

---
+++ tests/twitter_tests/test_twitter_auth_controller.py
@@ -0,0 +1,45 @@
+'''
+Cesar Guerrero
+09/27/23
+
+Testing our Twitter Authorization Controller
+'''
+
+import pytest
+from flask import session
+from unittest.mock import AsyncMock, patch
+
+#Testing our 'Login with Twitter' Route
+class TestTwittterLoginClass:
+
+    @patch('analytics_api.services.twitter_auth_service.obtain_twitter_request_token', new=AsyncMock(return_value={"status_code": 200, "status_message":"OK", "oauth_ready": True, "oauth_token": "abc"}))
+    def test_twitter_login_response(self,client):
+        #You should not be able to call login with anything other than GET
+        assert client.post('/login/twitter').status_code == 405
+        assert client.get('/login/twitter').status_code == 200
+        #Testing that we correctly receiving a response from our function call
+        response = client.get('/login/twitter')
+        assert response.data == b'{"oauth_ready":true,"oauth_token":"abc","status_code":200,"status_message":"OK"}\n'
+
+
+#Testing our Callback for the Twitter Oauth Flow
+class TestTwitterCallbackClass:
+
+    #Notice we don't need to mock our service function as it will never be called
+    def test_invalid_oauth_token_callback(self, client):
+        with client.session_transaction() as session:
+            #Setting up session variables
+            session['twitter_oauth_token'] = "abc123" 
+        #OAuth Token Mismatch
+        response = client.get('/callback/twitter?oauth_token=%s&oauth_verifier=%s' %("456def", "789xyz"))
+        assert response.data == b'Unauthorized'
+        assert response.status_code == 401
+
+    @patch('analytics_api.services.twitter_auth_service.obtain_twitter_access_token', new=AsyncMock(return_value={'oauth_approved': True, "status_code": 200, 'status_message': "OK"}))
+    def test_twitter_callback_response(self, client):
+        with client.session_transaction() as session:
+            #Setting up session variables
+            session['twitter_oauth_token'] = "abc123" 
+            session['twitter_oauth_token_secret'] = "456def"
+        response = client.get('/callback/twitter?oauth_token=%s&oauth_verifier=%s' %("abc123", "789xyz"))
+        assert response.data == b'{"oauth_approved":true,"status_code":200,"status_message":"OK"}\n'
